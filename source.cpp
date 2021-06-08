61ddb95568949747f05e1905216298d27b149f99
yaito3014 <ykakeyama3014@gmail.com>
2021-06-08 15:58:22 +0900

    depth feature and adopt cxxopts for parse options

--- source.cpp
+++ source.cpp
@@ -1,6 +1,3 @@
-#include <getopt.h>
-#include <unistd.h>
-
 #include <array>
 #include <compare>
 #include <concepts>
@@ -19,6 +16,7 @@
 #endif  // YK_ENABLE_PARALLEL
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "thirdparty/cxxopts.hpp"
 #include "thirdparty/stb_image_write.h"
 #include "yk/camera.hpp"
 #include "yk/color.hpp"
@@ -47,6 +45,7 @@ constexpr double aspect_ratio = 16.0 / 9.0;
 constexpr size_t image_width = YK_IMAGE_WIDTH;
 constexpr size_t image_height = static_cast<size_t>(image_width / aspect_ratio);
 constexpr size_t samples_per_pixel = YK_SPP;
+constexpr unsigned int max_depth = 50;
 
 }  // namespace constants
 
@@ -55,23 +54,30 @@ using image_t =
 
 template <std::floating_point T>
 constexpr color3b to_color3b(const color3<T>& from, size_t samples_per_pixel) {
-  return ((from / samples_per_pixel).clamped(0.0, 0.999) * 256)
-      .template to<uint8_t>();
+  auto [r, g, b] = from / samples_per_pixel;
+  color3d color = {
+      .r = sqrt(r),
+      .g = sqrt(g),
+      .b = sqrt(b),
+  };
+  return (color.clamped(0.0, 0.999) * 256).template to<uint8_t>();
 }
 
 template <concepts::arithmetic T, std::uniform_random_bit_generator Gen>
 constexpr vec3<T> random_vec_in_unit_sphere(Gen& gen) {
   return vec3<double>::random(gen, -1, 1).normalize() *
-         uniform_real_distribution<T>(0.01, 0.99)(gen);
+         uniform_real_distribution<T>(0.1, 0.99)(gen);
 }
 
 template <std::floating_point T, concepts::hittable<T> H,
           std::uniform_random_bit_generator Gen>
-constexpr color3d ray_color(const ray<T>& r, const H& world, Gen& gen) {
+constexpr color3d ray_color(const ray<T>& r, const H& world, unsigned int depth,
+                            Gen& gen) {
+  if (depth == 0) return color3d(0, 0, 0);
   hit_record<T> rec;
   if (world.hit(r, 0, std::numeric_limits<T>::infinity(), rec)) {
     pos3<T> target = rec.p + rec.normal + random_vec_in_unit_sphere<T>(gen);
-    return ray_color(ray<T>(rec.p, target - rec.p), world, gen) / 2;
+    return ray_color(ray<T>(rec.p, target - rec.p), world, depth - 1, gen) / 2;
   }
   auto t = (r.direction.normalized().y + 1.0) / 2;
   return (1.0 - t) * color3d(1.0, 1.0, 1.0) + t * color3d(0.5, 0.7, 1.0);
@@ -133,7 +139,8 @@ constexpr image_t render() {
                  auto u = (w + dist(gen)) / (constants::image_width - 1.0);
                  auto v = (constants::image_height - h - 1 + dist(gen)) /
                           (constants::image_height - 1.0);
-                 yk::color3d color = ray_color(cam.get_ray(u, v), world, gen);
+                 yk::color3d color = ray_color(cam.get_ray(u, v), world,
+                                               constants::max_depth, gen);
 #if YK_ENABLE_PARALLEL
                  std::lock_guard<std::mutex>{mtx},
 #endif  // YK_ENABLE_PARALLEL
@@ -165,29 +172,26 @@ int main(int argc, char* argv[]) {
   // handle command line args
 
   std::string filename;
-  std::vector<option> options = {
-      {.name = "help", .val = 'h'},
-      {.name = "verbose", .val = 'v'},
-      {.name = "output", .has_arg = required_argument, .val = 'o'}};
-  options.push_back({});  // options should be zero terminated;
-  switch (getopt_long(argc, argv, ":hvo:", options.data(), NULL)) {
-    case 'h':
-      std::cout << "available options : " << '\n'
-                << "-h, --help          : help message" << '\n'
-                << "-v, --verbose       : verbose" << '\n'
-                << "-o, --output[=file] : filename to output" << std::endl;
-      break;
-    case 'v':
-      ++yk::verbose;
-      break;
-    case 'o':
-      filename = optarg;
-      break;
-    case -1:
-      std::cerr << "unknown option : " << char(optopt) << std::endl;
-      exit(EXIT_FAILURE);
-      break;
+  cxxopts::Options options("raytrace", "raytracing program");
+  options.add_options()("h,help", "print usage")(
+      "o,output", "filename of output",
+      cxxopts::value<
+          std::string>())("v,verbose",
+                          "verbose output")("verbose-level",
+                                            "set verbose level",
+                                            cxxopts::value<
+                                                std::vector<size_t>>());
+  options.parse_positional({"output", "positional"});
+  auto parsed = options.parse(argc, argv);
+  if (parsed.count("help") || !parsed.count("output")) {
+    std::cout << options.help() << std::endl;
+    exit(EXIT_SUCCESS);
   }
+  if (parsed.count("verbose") && !yk::verbose) ++yk::verbose;
+  if (parsed.count("verbose-level"))
+    yk::verbose = parsed["verbose-level"].as<std::vector<size_t>>().back();
+
+  filename = parsed["output"].as<std::string>();
 
   // rendering
   YK_CONSTEXPR yk::image_t image = yk::render();
